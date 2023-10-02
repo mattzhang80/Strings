1d7960cf682f73d2c6ae01bd82deb0f3361dbc1e
matthewzhang <mz8189@princeton.edu>
2023-10-01 22:23:09 -0400

    edits

--- stra.c
+++ stra.c
@@ -14,8 +14,8 @@ size_t Str_getLength(const char pcSrc[])
 /*Create a new string that is a copy of an existing string. */
 char *Str_copy(char pcDest[], const char pcSrc[])
 {
-   assert(pcSrc != NULL && pcDest != NULL);
    size_t u;
+   assert(pcSrc != NULL && pcDest != NULL);
    for (u = 0; pcSrc[u] != '\0'; u++)
       pcDest[u] = pcSrc[u];
    pcDest[u] = '\0';
@@ -25,20 +25,20 @@ char *Str_copy(char pcDest[], const char pcSrc[])
 /*Concatenate two strings together. */
 char *Str_concat(char pcDest[], const char pcSrc[])
 {
-   assert(pcSrc != NULL && pcDest != NULL);
-   size_t destLength = Str_getLength(pcDest);
-   size_t u;
-   for (u = 0; pcSrc[u] != '\0'; u++)
+    size_t destLength = Str_getLength(pcDest);
+    size_t u;
+    assert(pcSrc != NULL && pcDest != NULL);
+    for (u = 0; pcSrc[u] != '\0'; u++)
       pcDest[destLength + u] = pcSrc[u];
-   pcDest[destLength + u] = '\0';
-   return pcDest;
+    pcDest[destLength + u] = '\0';
+    return pcDest;
 }
 
 /*Compare two strings. */
 int Str_compare(const char pcStr1[], const char pcStr2[])
 {
-   assert(pcStr1 != NULL && pcStr2 != NULL);
    size_t u;
+   assert(pcStr1 != NULL && pcStr2 != NULL);
    while (pcStr1[u] == pcStr2[u]) {
         if (pcStr1[u] == '\0') 
             return 0; 
@@ -50,9 +50,8 @@ int Str_compare(const char pcStr1[], const char pcStr2[])
 /*Search for a substring in a string. */
 const char *Str_search(const char pcHaystack[], const char pcNeedle[]) 
 {
+   size_t u, u2;
    assert(pcHaystack != NULL && pcNeedle != NULL);
-   size_t u;
-   size_t u2;
    for (u = 0; pcHaystack[u] != '\0'; u++) {
       for (u2 = 0; pcNeedle[u2] != '\0'; u2++) {
          if (pcHaystack[u + u2] != pcNeedle[u2])
